1de51072f711a1847ee45dacb6546ac27f641a81
Badder <badder.marzocchi@cern.ch>
2020-11-05 15:38:04 +0100

    scale datamix to data in training

--- forH4G/CatTrainMVA.py
+++ forH4G/CatTrainMVA.py
@@ -12,7 +12,7 @@ Cut_Signal = '(pho1_pt > 30 && pho2_pt > 18 && pho3_pt > 15 && pho4_pt > 15 && a
 Cut_Background = '(pho1_pt > 30 && pho2_pt > 18 && pho3_pt > 15 && pho4_pt > 15 && abs(pho1_eta) < 2.5 && abs(pho2_eta) < 2.5 && abs(pho3_eta) < 2.5 && abs(pho4_eta) < 2.5 && (abs(pho1_eta) < 1.4442 || abs(pho1_eta) > 1.566) && (abs(pho2_eta) < 1.4442 || abs(pho2_eta) > 1.566) && (abs(pho3_eta) < 1.4442 || abs(pho3_eta) > 1.566) && (abs(pho4_eta) < 1.4442 || abs(pho4_eta) > 1.566) && pho1_electronveto==1 && pho2_electronveto==1 && pho3_electronveto==1 && pho4_electronveto==1 && tp_mass > 110 && tp_mass <180 && pho1_MVA > -999. && pho2_MVA > -999. && pho3_MVA > -999. && pho4_MVA > -999)'
 
 #sig_list = [5,10,15,20,25,30,35,40,45,50,55,60]
-sig_list = [15,25,35,45,60]
+sig_list = [60]
 sig_nums_2016 = []
 sig_nums_2017 = []
 sig_nums_2018 = []
@@ -24,42 +24,45 @@ for mass in sig_list:
 
    sig_file_2016_tmp = ROOT.TChain()
    tree_name_2016 = '/eos/user/t/twamorka/h4g_fullRun2/withSystematics/2016/hadd/signal_m_'+str(mass)+'.root/tagsDumper/trees/SUSYGluGluToHToAA_AToGG_M_'+str(mass)+'_TuneCUETP8M1_13TeV_pythia8_13TeV_H4GTag_0'
-   #tree_name_2016 = '/eos/user/t/twamorka/h4g_fullRun2/withSystematics/2016/hadd/signal_m_'+str(mass)+'_genMass.root/SUSYGluGluToHToAA_AToGG_M_'+str(mass)+'_TuneCUETP8M1_13TeV_pythia8_13TeV_H4GTag_0'
    sig_file_2016_tmp.AddFile(tree_name_2016)
    sig_file_2016.append(sig_file_2016_tmp)      
    sig_nums_2016.append(float(sig_file_2016_tmp.GetEntries(Cut_Signal))) 
    
    sig_file_2017_tmp = ROOT.TChain()
    tree_name_2017 = '/eos/user/t/twamorka/h4g_fullRun2/withSystematics/2017/hadd/signal_m_'+str(mass)+'.root/tagsDumper/trees/SUSYGluGluToHToAA_AToGG_M_'+str(mass)+'_TuneCP5_13TeV_pythia8_13TeV_H4GTag_0'
-   #tree_name_2017 = '/eos/user/t/twamorka/h4g_fullRun2/withSystematics/2017/hadd/signal_m_'+str(mass)+'_genMass.root/SUSYGluGluToHToAA_AToGG_M_'+str(mass)+'_TuneCP5_13TeV_pythia8_13TeV_H4GTag_0'
    sig_file_2017_tmp.AddFile(tree_name_2017)
    sig_file_2017.append(sig_file_2017_tmp)      
    sig_nums_2017.append(float(sig_file_2017_tmp.GetEntries(Cut_Signal))) 
 
    sig_file_2018_tmp = ROOT.TChain()
    tree_name_2018 = '/eos/user/t/twamorka/h4g_fullRun2/withSystematics/2018/hadd/signal_m_'+str(mass)+'.root/tagsDumper/trees/HAHMHToAA_AToGG_MA_'+str(mass)+'GeV_TuneCP5_PSweights_13TeV_madgraph_pythia8_13TeV_H4GTag_0'
-   #tree_name_2018 = '/eos/user/t/twamorka/h4g_fullRun2/withSystematics/2018/hadd/signal_m_'+str(mass)+'_genMass.root/HAHMHToAA_AToGG_MA_'+str(mass)+'GeV_TuneCP5_PSweights_13TeV_madgraph_pythia8_13TeV_H4GTag_0'
    sig_file_2018_tmp.AddFile(tree_name_2018)
    sig_file_2018.append(sig_file_2018_tmp)      
    sig_nums_2018.append(float(sig_file_2018_tmp.GetEntries(Cut_Signal))) 
 
 lumi_2016 = 35.9
-bkg_file_2016 = ROOT.TChain()
-bkg_file_2016.AddFile('/eos/user/t/twamorka/h4g_fullRun2/withSystematics/2016/hadd/data_mix_weight_v3.root/Data_13TeV_H4GTag_0')
-#bkg_file_2016.AddFile('/eos/user/t/twamorka/h4g_fullRun2/withSystematics/2016/hadd/data_mix_weight_v3_genMass.root/Data_13TeV_H4GTag_0')
-norm_2016 = float(bkg_file_2016.GetEntries(Cut_Background))
+data_file_2016 = ROOT.TChain()
+data_file_2016.AddFile('/eos/user/t/twamorka/h4g_fullRun2/withSystematics/2016/hadd/data_2016.root/tagsDumper/trees/Data_13TeV_H4GTag_0')
+datamix_file_2016 = ROOT.TChain()
+datamix_file_2016.AddFile('/eos/user/t/twamorka/h4g_fullRun2/withSystematics/2016/hadd/data_mix_weight_v8.root/Data_13TeV_H4GTag_0')
+data_2016 = float(data_file_2016.GetEntries(Cut_Background))
+datamix_2016 = float(datamix_file_2016.GetEntries(Cut_Background))
 
 lumi_2017 = 41.5
-bkg_file_2017 = ROOT.TChain()
-bkg_file_2017.AddFile('/eos/user/t/twamorka/h4g_fullRun2/withSystematics/2017/hadd/data_mix_weight_v3.root/Data_13TeV_H4GTag_0')
-#bkg_file_2017.AddFile('/eos/user/t/twamorka/h4g_fullRun2/withSystematics/2017/hadd/data_mix_weight_v3_genMass.root/Data_13TeV_H4GTag_0')
-norm_2017 = float(bkg_file_2017.GetEntries(Cut_Background))
+data_file_2017 = ROOT.TChain()
+data_file_2017.AddFile('/eos/user/t/twamorka/h4g_fullRun2/withSystematics/2017/hadd/data_2017.root/tagsDumper/trees/Data_13TeV_H4GTag_0')
+datamix_file_2017 = ROOT.TChain()
+datamix_file_2017.AddFile('/eos/user/t/twamorka/h4g_fullRun2/withSystematics/2017/hadd/data_mix_weight_v8.root/Data_13TeV_H4GTag_0')
+data_2017 = float(data_file_2017.GetEntries(Cut_Background))
+datamix_2017 = float(datamix_file_2017.GetEntries(Cut_Background))
 
 lumi_2018 = 54.38
-bkg_file_2018 = ROOT.TChain()
-bkg_file_2018.AddFile('/eos/user/t/twamorka/h4g_fullRun2/withSystematics/2018/hadd/data_mix_weight_v3.root/Data_13TeV_H4GTag_0')
-#bkg_file_2018.AddFile('/eos/user/t/twamorka/h4g_fullRun2/withSystematics/2018/hadd/data_mix_weight_v3_genMass.root/Data_13TeV_H4GTag_0')
-norm_2018 = float(bkg_file_2018.GetEntries(Cut_Background))
+data_file_2018 = ROOT.TChain()
+data_file_2018.AddFile('/eos/user/t/twamorka/h4g_fullRun2/withSystematics/2018/hadd/data_2018.root/tagsDumper/trees/Data_13TeV_H4GTag_0')
+datamix_file_2018 = ROOT.TChain()
+datamix_file_2018.AddFile('/eos/user/t/twamorka/h4g_fullRun2/withSystematics/2018/hadd/data_mix_weight_v8.root/Data_13TeV_H4GTag_0')
+data_2018 = float(data_file_2018.GetEntries(Cut_Background))
+datamix_2018 = float(datamix_file_2018.GetEntries(Cut_Background))
 
 f_out = ROOT.TFile(output+'.root','RECREATE')
 ROOT.TMVA.Tools.Instance()
@@ -74,19 +77,19 @@ for x in mvaVars:
    dataloader.AddVariable(x,"F")
 
 for i,mass in enumerate(sig_list):
-   #print "---> Signal normalization 2016: ",i,mass,float(sig_nums_2016[i])
-   #dataloader.AddSignalTree(sig_file_2016[i],lumi_2016/float(sig_nums_2016[i]))
-   #print "---> Signal normalization 2017: ",i,mass,float(sig_nums_2017[i])
-   #dataloader.AddSignalTree(sig_file_2017[i],lumi_2017/float(sig_nums_2017[i])) 
+   print "---> Signal normalization 2016: ",i,mass,float(sig_nums_2016[i])
+   dataloader.AddSignalTree(sig_file_2016[i],lumi_2016/float(sig_nums_2016[i]))
+   print "---> Signal normalization 2017: ",i,mass,float(sig_nums_2017[i])
+   dataloader.AddSignalTree(sig_file_2017[i],lumi_2017/float(sig_nums_2017[i])) 
    print "---> Signal normalization 2018: ",i,mass,float(sig_nums_2018[i])
    dataloader.AddSignalTree(sig_file_2018[i],lumi_2018/float(sig_nums_2018[i]))
 
-#print "---> Background normalization 2016: ",norm_2016
-#dataloader.AddBackgroundTree(bkg_file_2016,lumi_2016/norm_2016)
-#print "---> Background normalization 2017: ",norm_2017
-#dataloader.AddBackgroundTree(bkg_file_2017,lumi_2017/norm_2017)
-print "---> Background normalization 2018: ",norm_2018
-dataloader.AddBackgroundTree(bkg_file_2018,lumi_2018/norm_2018)
+print "---> Background normalization 2016: ",datamix_2016/data_2016
+dataloader.AddBackgroundTree(data_file_2016,datamix_2016/data_2016)
+print "---> Background normalization 2017: ",datamix_2017/data_2017
+dataloader.AddBackgroundTree(data_file_2017,datamix_2017/data_2017)
+print "---> Background normalization 2018: ",datamix_2018/data_2018
+dataloader.AddBackgroundTree(data_file_2018,datamix_2018/data_2018)
 
 sigCut = ROOT.TCut(Cut_Signal)
 bkgCut = ROOT.TCut(Cut_Background)
